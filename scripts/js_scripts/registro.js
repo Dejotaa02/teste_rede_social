1cf4530ca37a1f6a00faeceac4adcd8907dbd047
Paulo H. <paulohtolotti@alumni.usp.br>
2025-06-15 10:08:48 -0300

    fix: remoção de console log de teste

--- scripts/js_scripts/registro.js
+++ scripts/js_scripts/registro.js
@@ -3,7 +3,6 @@ let curLattes = document.querySelector("#lattes");
 curLattes.style.display = "none";
 
 checkbox.addEventListener("click", (ev) => {
-    console.log(curLattes.getAttribute("display"))
     if (curLattes.style.display === 'none') {
         curLattes.style.display = "inline";
     } else {
