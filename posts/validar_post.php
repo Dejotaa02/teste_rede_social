b9f7294e52cf543fd368646444b1e0c2b897250e
Paulo H. <paulohtolotti@alumni.usp.br>
2025-06-15 10:07:35 -0300

    feat: adição do endpoint de validar post

--- posts/validar_post.php
+++ posts/validar_post.php
@@ -5,11 +5,13 @@ if (session_status() === PHP_SESSION_NONE) session_start();
 require_once __DIR__ . '/../inc/db_database.php';
 $tipo = $_SESSION['tipo'] ?? null;
 
+
 //Usuário não pode validar;
 if($tipo != "especialista"){
     json_encode([]);
     exit;
 }
+
 $db = new Database();
 $usuario = $_SESSION['usuario'] ?? null;
 if (!$usuario || $_SERVER['REQUEST_METHOD'] !== 'POST') {
